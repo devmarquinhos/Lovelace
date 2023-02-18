b8c64e55c47c523655a60f5f1d7e13dc2dc46e6e
marquin.e <marcos.emanuel.04@gmail.com>
2023-02-18 10:11:05 -0300

    Style: Timer buttons have the same style | Add: Timer script added | Add: Page for the practice created
    Now all the buttons of the timer, on practice page, has the same style, including the hover effects, they also is functional, that means they can start, pause and clear the time. The Practice page still only having the timer, needs to make more similar to the Figma's prototype

---
+++ src/js/timer.js
@@ -0,0 +1,56 @@
+"use strict"
+let hours = 0
+let minutes = 0
+let seconds = 0
+let time = 1000 // Quantidade milésimos que um segundo possue
+let timer
+let timerStatus
+
+function start() {
+    switch (timerStatus) {
+        case "play":
+            return
+
+        default:
+            timerStatus = "play"
+            timer = setInterval(() => {
+                timerStart()
+            }, time); 
+            break
+    }
+}
+
+function stop() {
+    timerStatus = "stop"
+    clearInterval(timer)
+
+}
+
+function clearTimer(){
+    clearInterval(timer)
+    hours = 0
+    minutes = 0
+    seconds = 0
+
+    document.getElementById('timer').innerText = '00:00:00'
+}
+
+function timerStart(){
+    seconds++
+
+    if (seconds == 60) {
+        seconds = 0
+        minutes++
+
+        if (minutes == 60) {
+            minutes = 0
+            hours++
+        }
+    }
+
+    let formatTimer = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds)
+
+    document.getElementById('timer').innerText = formatTimer
+
+    return formatTimer
+}
