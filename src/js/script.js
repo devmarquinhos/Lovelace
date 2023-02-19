eefa65d8967ffab4b77fc2d4008bf4b53fe5185b
marquin.e <marcos.emanuel.04@gmail.com>
2023-02-19 20:40:23 -0300

    New: More texts, unique script file
    Now we have two more texts to use, this branch is specifically to test a randomic way to change the text from the container. I use the randomInt function available at MDN JavaScript guide and its works, now I want to have a solution that prevent the user of getting the same past text and always have a new text to read

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,77 @@
+"use strict"
+let hours = 0
+let minutes = 0
+let seconds = 0
+let time = 1000 // Quantidade milésimos que um segundo possue
+let timer
+let timerStatus
+
+function start() {
+    switch (timerStatus) {
+        case "play":
+            return
+        default:
+            timerStatus = "play"
+            timer = setInterval(() => {
+                timerStart()
+            }, time); 
+            break
+    }
+}
+
+function stop() {
+    timerStatus = "stop"
+    clearInterval(timer)
+}
+
+function clearTimer(){
+    clearInterval(timer)
+    hours = 0
+    minutes = 0
+    seconds = 0
+
+    document.getElementById('timer').innerText = '00:00:00'
+}
+
+function timerStart(){
+    seconds++
+
+    if (seconds == 60) {
+        seconds = 0
+        minutes++
+
+        if (minutes == 60) {
+            minutes = 0
+            hours++
+        }
+    }
+
+    let formatTimer = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds)
+    document.getElementById('timer').innerText = formatTimer
+
+    return formatTimer
+}
+
 function changeText(){
-    document.getElementById("containerContent").setAttribute("src", '/assets/ourvacation.html')
+    stop()
+    clearTimer()
+    start()
+    const textAddress = [
+        '/assets/ourvacation.html',
+        '/assets/myday.html',
+        '/assets/mywonderfulfamily.html',
+        '/assets/mynameisjohn.html',
+        '/assets/agreatsummervacation.html'
+    ]
+    
+    document.getElementById("containerContent").setAttribute("src", textAddress[getRandomInt()])
+    console.log(getRandomInt())
+}
+
+function getRandomInt() {
+    var min = Math.ceil(1);
+    var max = Math.floor(5);
+
+    // The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min) + min)
 }
