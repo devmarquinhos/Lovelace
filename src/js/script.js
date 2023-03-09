aa8646bdb7abd1766b52f1ab89fd043463ae6ce2
Bobzilarx <danielalves33147@gmail.com>
2023-03-08 23:31:51 -0300

    Calculo de Pontuação Funcional

--- src/js/script.js
+++ src/js/script.js
@@ -8,14 +8,12 @@ let time = 1000 // Quantidade milésimos que um segundo possue
 let timer
 let timerStatus = "stop"
 let practiceText
-let sec,deCont,decress,minTime
-
+let sec,deCont,decress,score
+let ponto_por_palavra,qtd_de_palavras_chave=5,acertos,resposta
 
 function start() {
     switch (timerStatus) {
         case "play":
-            return
-        case "stop":
             return
         default:
             timerStatus = "play"
@@ -29,18 +27,6 @@ function start() {
 function stop() {
     timerStatus = "stop"
     clearInterval(timer)
-}
-
-function getPoints(){
-    sec = seconds + (60*minutes) + (3600*hours)
-
-    if(sec <= minTime){
-        deCont = 0
-    }
-
-    deCont = (sec/30) - minTime
-
-    decress = deCont*15
 }
 
 
@@ -108,6 +94,7 @@ function changeText(practiceText){
     console.log("Segundo console.log: " + pos)
 }
 
+
 // Fechar modal e parar o tempo
 function sendAnswers() {
     stop()
@@ -115,20 +102,78 @@ function sendAnswers() {
     let resContainer = document.getElementById("resultContainer")
     let answerContainer = document.getElementById("submitAnswer").value
 
+  
+    
     switch (answerContainer) {
         case "":
             alert("Ei! O campo de resposta precisa estar preenchido!")
             console.log("Campo vazio.")
             break;
-    
+
         default:
-            console.log(answerContainer)
-            resContainer.classList.add("show")
-            resContainer.addEventListener('click', (e) => {
-                if (e.target.id =='dontSavePdf') {
-                    resContainer.classList.remove("show")
-                }
-            })
+
+    let palavra_acertada = 0,qtd_de_palavras_chave = 5
+    let minTime = 60
+    let rank
+
+    if(answerContainer.includes("A")){
+        palavra_acertada ++
+    }
+    if(answerContainer.includes("resposta")){
+        palavra_acertada ++
+    }
+    if(answerContainer.includes("certa")){
+        palavra_acertada ++
+    }
+    if(answerContainer.includes("e")){
+        palavra_acertada ++
+    }
+    if(answerContainer.includes("essa")){
+        palavra_acertada ++
+    }
+
+    ponto_por_palavra = 100 / qtd_de_palavras_chave
+    acertos = ponto_por_palavra * palavra_acertada
+
+    sec = seconds + (60*minutes) + (3600*hours)
+
+    if(sec <= minTime){
+        score = acertos
+    }else{
+        deCont = sec - minTime
+        decress = (deCont / 30)*10
+        score = acertos - decress 
+    }
+
+    if(score <= 0){
+       rank = "Ruim"
+    }else if(score <= 20){
+       rank = "Baixo"
+    }else if(score <= 40){
+       rank = "Bom"
+    }else if(score <= 60){
+       rank = "Medio"
+    }else if(score <= 80){
+       rank = "Alto"
+    }else if(score <= 100){
+       rank = "Advançado"
+    }
+
+
+    document.getElementById("score").innerHTML = Math.round(score);
+    document.getElementById("level").innerHTML = rank;
+
+    console.log(score)
+
+    resContainer.classList.add("show")
+    resContainer.addEventListener('click', (e) => {
+        if (e.target.id =='dontSavePdf') {
+            resContainer.classList.remove("show")
+            changeText(practiceText)
+            answerContainer = document.getElementById("submitAnswer").value=''
+        }
+    })
+
             break;
     }
 }
