684764c5fe00849981fe4dfdb439fe52684da745
Bobzilarx <danielalves33147@gmail.com>
2023-03-19 19:19:27 -0300

    change input to buttons

--- src/js/script.js
+++ src/js/script.js
@@ -295,7 +295,7 @@ function calc(answerContainer){
     document.getElementById("level").innerHTML = rank;
     document.getElementById("txt").innerHTML = practiceText[pre].textTitle;
     document.getElementById("time").innerHTML = document.getElementById("timer").innerHTML;
-    document.getElementById("login").innerHTML = login;
+    document.getElementById("login").innerHTML = "admin";
     console.log(palavra_acertada)
 }
 
